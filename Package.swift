e3838e3d0fad5aba7b091caf43d4467859cd31fe
Derek Clarkson <d4rkf1br3@gmail.com>
2018-09-21 17:54:23 +1000

    Unediting xcodeproj and updating dependencies

--- Package.swift
+++ Package.swift
@@ -9,11 +9,12 @@ let package = Package(
         .executable(name: "wrench", targets: ["Wrench"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kareman/SwiftShell", from: "4.0.0"),
-        .package(url: "https://github.com/JohnSundell/Files", from: "2.2.0"),
-        .package(url: "https://github.com/tuist/xcodeproj", from: "6.0.0"),
+        .package(url: "https://github.com/kareman/SwiftShell", .branch("master")),
+        .package(url: "https://github.com/JohnSundell/Files", .branch("master")),
+//        .package(url: "https://github.com/tuist/xcodeproj", from: "6.0.0"),
+        .package(url: "https://github.com/drekka/xcodeproj", .branch("feature/project-sorting")),
         //        .package(url: "https://github.com/kareman/FileSmith", from: "0.2.0"),
-        .package(url: "https://github.com/apple/swift-package-manager", from: "0.1.0"),
+        .package(url: "https://github.com/apple/swift-package-manager", .branch("master")),
     ],
     targets: [
         .target(
