a8162937ab93661e61c3ffe2676a1bb816be2962
Derek Clarkson <d4rkf1br3@gmail.com>
2018-10-11 15:24:37 +1100

    Untracking gitmodules

--- Package.swift
+++ Package.swift
@@ -7,12 +7,13 @@ let package = Package(
     name: "Shelly",
     products: [
         .library(name: "Shelly", targets: ["Shelly"]),
-    ],
+        ],
     dependencies: [
-    .package(url: "https://github.com/Quick/Nimble", from: "7.0.0"),
+        .package(url: "https://github.com/Quick/Nimble", from: "7.0.0"),
         .package(url: "https://github.com/kareman/SwiftShell", .branch("master")),
-        .package(url: "https://github.com/apple/swift-package-manager", from: "0.3.0"),
-    ],
+        .package(url: "https://github.com/drekka/swift-package-manager", .branch("feature/everything")),
+//        .package(url: "https://github.com/apple/swift-package-manager", from: "0.3.0"),
+        ],
     targets: [
         .target(
             name: "Shelly",
@@ -22,5 +23,5 @@ let package = Package(
             name: "ShellyTests",
             dependencies: ["Shelly", "Nimble"]
         ),
-    ]
+        ]
 )
